6a561e162b7a89c0fe2d30fa6f8c34ccd742bed0
bwawczak80 <35830914+bwawczak80@users.noreply.github.com>
2019-02-21 17:17:10 -0600

    fixed errors

--- app/src/main/java/net/wawczak/brian/cityguidebrianw/MainActivity.java
+++ app/src/main/java/net/wawczak/brian/cityguidebrianw/MainActivity.java
@@ -1,22 +1,24 @@
 package net.wawczak.brian.cityguidebrianw;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
 
         String[] attraction={"Art Institute of Chicago", "Magnificent Mile", "Willis Tower", "Navy Pier", "Water Tower"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, attraction);
+        setListAdapter(new ArrayAdapter<>(this, R.layout.activity_main, R.id.travel, attraction));
+    }
 
-        ListView listView = findViewById(android.R.id.list);
-        listView.setAdapter(adapter);
+    protected void onListItemClick(ListView l, View v, int position, long id){
+
     }
 }
